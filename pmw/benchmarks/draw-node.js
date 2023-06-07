3d17a72e30d0bd2a8a64d281b8b7cf01fb5e8df9
Kalane <vkaelin@hey.com>
2023-06-07 11:49:29 +0200

    feat: add small node benchmark

---
+++ pmw/benchmarks/draw-node.js
@@ -0,0 +1,42 @@
+module.exports = { draw };
+
+const NB_LINES = 20;
+let width, height;
+
+function random(max = 100) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomX() {
+  return random(width);
+}
+
+function randomY() {
+  return random(height);
+}
+
+async function line(page) {
+  const startX = randomX();
+  const startY = randomY();
+
+  const endX = randomX();
+  const endY = randomY();
+
+  await page.mouse.move(startX, startY);
+  await page.mouse.down();
+
+  await page.mouse.move(endX, endY);
+  await page.mouse.up();
+}
+
+async function draw(page) {
+  await page.goto("https://pmw-node.netlify.app");
+
+  const dimensions = page.viewportSize();
+  width = dimensions.width;
+  height = dimensions.height;
+
+  for (let i = 0; i < NB_LINES; i++) {
+    await line(page);
+  }
+}
